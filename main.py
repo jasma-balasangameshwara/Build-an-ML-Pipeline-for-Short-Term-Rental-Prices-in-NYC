acf21e8dbf27556b143e47bc928998c999616426
jasma-balasangameshwara <jasma.balasangameshwara@gmail.com>
2022-01-09 11:07:35 +0530

    main.py updated

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+    #    "test_regression_model"
 ]
 
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
-
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
@@ -58,6 +57,7 @@ def go(config: DictConfig):
                 parameters={
                     "input_artifact": "sample.csv:latest",
                     "output_artifact": "basic_cleaned_data.csv",
+                    "output_type": "basic_cleaned_data",
                     "min_price": config['etl']['min_price'],
                     "max_price": config['etl']['max_price'],
                 },
@@ -67,7 +67,6 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            os.environ["WANDB_PROJECT"] = config["data_check"]["project_name"]
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                 "main",
@@ -84,7 +83,6 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            os.environ["WANDB_PROJECT"] = config["data_check"]["project_name"]
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
@@ -109,16 +107,32 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export",
+                },
+            )
 
         if "test_regression_model" in active_steps:
-
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": config["test_regression_model"]["mlflow_model"],
+                    "test_dataset": config['test_regression_model']['test_dataset']
+                },
+            )
 
 
 if __name__ == "__main__":
